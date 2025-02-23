b3ef9c6ca6325d4b958210caf8dda7c6e45f983a
cici thomson <cici.thomson@temporal.io>
2025-02-23 14:26:22 -0500

    WIP for entity workflow

---
+++ config.py
@@ -0,0 +1,39 @@
+import os
+from urllib.parse import urlparse
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def get_config() -> dict:
+    web_url = urlparse(os.getenv('PUBLIC_WEB_URL'))
+    return {
+        'name': 'Temporal Newsfeed Notification Demo',
+        'temporal': {
+            'connection': {
+                'target': os.getenv('TEMPORAL_CONNECTION_TARGET'),
+                'namespace': os.getenv('TEMPORAL_CONNECTION_NAMESPACE'),
+                'mtls': {
+                    'key_file': os.getenv('TEMPORAL_MTLS_TLS_KEY'),
+                    'cert_chain_file': os.getenv('TEMPORAL_MTLS_TLS_CERT'),
+                },
+                # 'web_port': os.getenv('TEMPORAL_CONNECTION_WEB_PORT')
+            },
+            'worker': {
+                'task_queue': os.getenv('TEMPORAL_TASK_QUEUE')
+            },
+            # 'api': {
+            #     'port': os.getenv('CALLER_API_PORT')
+            # }
+        },
+        'web': {
+            'url': web_url,
+            'connection': {
+                'mtls': {
+                    'key_file': os.getenv('WEB_CONNECTION_MTLS_KEY_FILE'),
+                    'cert_chain_file': os.getenv('WEB_CONNECTION_MTLS_CERT_CHAIN_FILE')
+                }
+            }
+        }
+    }
